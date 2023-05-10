9cb2a4418085fd35f18f63ff9499cae83bcf5ff8
Bartlomiej <szybiakbartlomiej@gmail.com>
2023-05-10 21:41:25 +0200

    10.05.2023

--- src/main/java/org/example/basic/Kata.java
+++ src/main/java/org/example/basic/Kata.java
@@ -16,7 +16,15 @@ public class Kata {
 //        System.out.println(kata.max(new int[]{-1, 4, 5, 6, -100}));
 //        System.out.println(ipsBetween("10.0.0.0", "10.0.0.50"));
 //        squareDigits(9119);
-        System.out.println(squareDigitsSt(9119));
+//        System.out.println(squareDigitsSt(9119));
+//        System.out.println(getGrade(95,90,93));
+//updateLight("green");
+         int[] ints = new int[]{9,5,3,7,8,2,1};
+
+         for (int x : tabSort(ints,0,ints.length-1)){
+             System.out.println(x);
+         }
+
 
     }
 
@@ -75,16 +83,60 @@ public class Kata {
 
     public static int squareDigitsSt(int n) {
 
-        String collect = String.valueOf(n)
+       return Integer.parseInt( String.valueOf(n)
                 .chars()
-                .map(t -> Integer.parseInt(String.valueOf((char) t)))
-                .map(i -> i * i)
+                .map(v->Integer.parseInt(String.valueOf((char)v)))
+                .map(i->i*i)
                 .mapToObj(String::valueOf)
-                .collect(Collectors.joining(""));
-
-
-        return Integer.parseInt(collect);
+                .collect(Collectors.joining("")));
     }
 
+    public static char getGrade(int s1, int s2, int s3) {
+        int avr = (s1 + s2 + s3)/3;
+        return avr>=90?'A':avr>=80?'B':avr>=70?'C':avr>=60?'D':'F';
+    }
+
+    public static String updateLight(String current) {
+
+        switch (current){
+            case "red" : return "green";
+            case "green" : return "yellow";
+            case "yellow" : return "red";
+            default:throw new IllegalArgumentException();
+        }
+
+    }
+
+    public static int[] tabSort(int[]tab, int low, int high ){
+
+        if (low<high){
+            int pivotIndex = partition(tab, low, high);
+            tabSort(tab,low,pivotIndex);
+            tabSort(tab,pivotIndex+1,high);
+
+        }
+
+return tab;
+    }
+
+    private static int partition(int[] tab, int low, int high) {
+        int pivot = tab[low];
+        int q = high+1;
+        int p = low-1;
+        while (true){
+            do {
+                p++;
+            }while (tab[p]<pivot);
+            do {
+                q--;
+            }while (tab[q]>pivot);
+            if (p>=q){
+                return q;
+            }
+            int temp = tab[p];
+            tab[p] = tab[q];
+            tab[q]= temp;
+        }
+    }
 }
 
